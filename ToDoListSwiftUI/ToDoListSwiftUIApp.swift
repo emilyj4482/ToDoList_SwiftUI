b4f0cc4d17101df2aba73bb99e99cb2c36fe91ed
emilyj4482 <emilyj4482@gmail.com>
2023-05-01 21:43:27 +0900

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by EMILY on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
