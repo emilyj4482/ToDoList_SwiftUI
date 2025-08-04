7df8dc89b025359268036184e24b1e0cd8060b63
emilyj4482 <emilyj4482@gmail.com>
2025-08-05 04:04:51 +0900

    [feat] DataManager 구현

---
+++ ToDoList_SwiftUI/Helper/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  ToDoList_SwiftUI
+//
+//  Created by EMILY on 05/08/2025.
+//
+
+import Foundation
+
+struct Keys {
+    static let dataDirectoryName = "ToDoListAppData"
+    static let jsonFileName = "todo.json"
+}
---
+++ ToDoList_SwiftUI/Model/DataManager.swift
@@ -0,0 +1,81 @@
+//
+//  DataManager.swift
+//  ToDoList_SwiftUI
+//
+//  Created by EMILY on 2023/09/04.
+//  Refactored by EMILY on 2025/08/05.
+
+import Foundation
+
+// disk에 app data를 json 파일로 저장해주는 객체
+final class DataManager {
+    private let fileManager = FileManager.default
+    
+    init() {
+        createDirectoryIfNeeded()
+    }
+    
+    // json 파일로 todo data를 저장할 "ToDoListAppData" 폴더 생성
+    private func createDirectoryIfNeeded() {
+        guard let directoryURL = getAppDataDirectoryURL() else { return }
+        let path = directoryURL.path(percentEncoded: false)
+        
+        if !fileManager.fileExists(atPath: path) {
+            do {
+                try fileManager.createDirectory(atPath: path, withIntermediateDirectories: true)
+                print("[DataManager] ToDoListAppData Directory CREATED >>> \(path)")
+            } catch {
+                print("[DataManager] Failed to create directory: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // "ToDoListAppData" 디렉토리 경로를 반환하는 함수
+    private func getAppDataDirectoryURL() -> URL? {
+        guard let baseURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else { return nil }
+        let directoryURL = baseURL.appending(path: Keys.dataDirectoryName, directoryHint: .isDirectory)
+        
+        return directoryURL
+    }
+    
+    // json 파일 경로를 반환하는 함수
+    private func getJSONFileURL() -> URL? {
+        guard let directoryURL = getAppDataDirectoryURL() else { return nil }
+        let fileURL = directoryURL.appending(component: Keys.jsonFileName)
+        
+        return fileURL
+    }
+    
+    func getData<T: Decodable>() -> [T] {
+        guard
+            let url = getJSONFileURL(),
+            let data = try? Data(contentsOf: url)
+        else {
+            return []
+        }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode([T].self, from: data)
+            return decodedData
+        } catch {
+            print("[DataManager] Failed to decode data: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func saveData<T: Encodable>(_ data: [T]) {
+        guard let url = getJSONFileURL() else { return }
+        
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        
+        do {
+            let encodedData = try encoder.encode(data)
+            try encodedData.write(to: url, options: .atomic)    // 기존 파일이 있으면 덮어씀
+        } catch {
+            print("[DataManager] Failed to write data: \(error.localizedDescription)")
+        }
+    }
+}
